8fce6182f10436eb7a63e687165d4bc250067f84
norby <norbyte+github@gmail.com>
2024-01-13 18:27:10 +0100

    move ModMenuIntegration to root package

--- src/main/java/net/entityoutliner/ModMenuIntegration.java
+++ src/main/java/net/entityoutliner/ModMenuIntegration.java
@@ -1,7 +1,8 @@
-package net.entityoutliner.ui;
+package net.entityoutliner;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.entityoutliner.ui.EntitySelector;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
